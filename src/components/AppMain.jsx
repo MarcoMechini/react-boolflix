b50080c0b37336e76d592acbcd490ae65f7e082f
Marco Mechini <mechinimarco35@gmail.com>
2025-01-14 15:16:03 +0100

    feat: visualizzazione lista dei post con immagini

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,8 +1,24 @@
+import { useContext } from "react"
+import GlobalContext from "../context/GlobalContext"
 
 export default function AppMain() {
+
+    const { apiResp } = useContext(GlobalContext)
+    console.log('apiResp', apiResp);
+
+    const imgUrl = 'https://image.tmdb.org/t/p'
+
     return (
         <main>
-            <h1>sono main</h1>
+            <ul>
+                {apiResp && apiResp.map(curItem => (
+                    <li key={curItem.id}>
+                        <img src={`${imgUrl}/w200/${curItem.poster_path}`} alt="" />
+                        <h4>{curItem.title}</h4>
+                        <p>{curItem.overview}</p>
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
